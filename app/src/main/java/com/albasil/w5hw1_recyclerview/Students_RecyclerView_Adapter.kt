677bb77db1df1ac0ceb184cc279808cb6c5669f8
Basil19 <bojsl-9@hotmail.com>
2021-10-17 20:57:37 +0300

    WEEK4-DAY1->DONE

--- app/src/main/java/com/albasil/w5hw1_recyclerview/Students_RecyclerView_Adapter.kt
+++ app/src/main/java/com/albasil/w5hw1_recyclerview/Students_RecyclerView_Adapter.kt
@@ -3,6 +3,7 @@ package com.albasil.w5hw1_recyclerview
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -13,10 +14,12 @@ class Students_RecyclerView_Adapter(private val studentList:List<Students>):
         RecyclerView.Adapter<StudentAdapter.ViewHolder>(){
 
 
+    private var images = intArrayOf(R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,R.drawable.student,)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentAdapter.ViewHolder {
 
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.recycler_view, parent,false)
+            .inflate(R.layout.card_layout, parent,false)
+// or            .inflate(R.layout.recycler_view, parent,false)
 
         return  StudentAdapter.ViewHolder(view)
     }
@@ -30,6 +33,7 @@ class Students_RecyclerView_Adapter(private val studentList:List<Students>):
         val student=studentList[position]
         holder.id_student.text = " ID# ${student.id.toString()}"
         holder.name_studenyt.text = "${student.name} "
+        holder.id_image.setImageResource(images[position])
 
     }
 
@@ -46,16 +50,18 @@ class StudentAdapter{
     class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView),
 
         View.OnClickListener{
+        var id_image:ImageView
         val id_student: TextView = itemView.findViewById(R.id.id_textView)
         val name_studenyt: TextView = itemView.findViewById(R.id.name_textView)
 
 
         init {
             itemView.setOnClickListener(this)
+            id_image =itemView.findViewById(R.id._id_image)
         }
 
         override fun onClick(v: View?) {
-            Toast.makeText(itemView.context,"${id_student.text} student :", Toast.LENGTH_SHORT).show()
+            Toast.makeText(itemView.context,"${id_student.text} -> ${name_studenyt.text}", Toast.LENGTH_SHORT).show()
 
         }
     }
